bf1b6053bf7c0e5af081e67d59a51c70574cb9f4
Lallene Cedric <lallene2016@gmail.com>
2026-05-12 23:02:40 +0200

    sync: push all local project files.

--- src/app/api/prescriptions/dispense/route.ts
+++ src/app/api/prescriptions/dispense/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
+
+//test
 export async function POST(req: Request) {
   try {
     const body = await req.json();
